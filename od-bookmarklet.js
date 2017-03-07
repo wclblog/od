094ce2b4df4b5d4ad7f06e3ffd8939393f7cb3b0
wclblog <wclblog@gmail.com>
2017-03-08 11:00:28 +1300

    Update od-bookmarklet.js

--- od-bookmarklet.js
+++ od-bookmarklet.js
@@ -11,21 +11,22 @@ if (typeof jQuery == 'undefined') {
 function callback() {
 
 	var url = location.href;
-	
 	var id = location.pathname;
 	id = id.split('/');
-	id = id[2];
-	
+	var arrLength = id.length;
+	arrLength = arrLength - 1;
+	id = id[arrLength];
+
 	var json = window.OverDrive.mediaItems[id];
-	
+
 	var img = json.covers.cover150Wide.href;
 	var title = json.title;
 	var author = json.firstCreatorName;
 	var description = json.description;
 	var format = String(jQuery('.TitleDetailsHeading-formatBadge:eq(0)').text());
 	format = format.trim();
-	
-	
+
+
 	var html = '<a href="' + url + '"><img src="' + img + '" alt="Overdrive cover" class="alignleft" /></a> <a href="' + url + '">' + title + "</a>, " + author + ' (' + format + ')' + description + '<p>(Overdrive description)<\/p>';
 	prompt('ctrl+a ctrl+c to copy and paste', html);
 
